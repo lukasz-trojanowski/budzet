20f0330bcdec7014ecb6dc897a606156ff77746c
Łukasz Trojanowski <trojanowskil@gmail.com>
2021-09-13 20:58:49 +0200

    commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class IndexController extends AbstractController
+{
+
+  /**
+   * @Route("/")
+   */
+
+  public function index()
+  {
+    return $this->render('index/index.html.twig');
+  }
+}
